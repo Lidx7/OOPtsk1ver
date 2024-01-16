1df3c3381d2139385e145e0516847854ad4f368b
Lidx7 <155881749+Lidx7@users.noreply.github.com>
2024-01-16 19:50:24 +0200

    Update ConcretePlayer.java

--- sr7sui/src/ConcretePlayer.java
+++ sr7sui/src/ConcretePlayer.java
@@ -1,11 +1,25 @@
 public class ConcretePlayer implements Player{
+
+    private boolean isAttacker;
+    private int wins;
+
+    public ConcretePlayer(boolean isAttacker){
+        this.isAttacker = isAttacker;
+        this.wins = 0;
+
+    }
+
+    public void win(){
+        wins++;
+    }
+
     @Override
     public boolean isPlayerOne() {
-        return false;
+        return isAttacker;
     }
 
     @Override
     public int getWins() {
-        return 0;
+        return wins;
     }
 }
