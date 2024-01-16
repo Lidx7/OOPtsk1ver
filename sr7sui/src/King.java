b2c22e52f9c8ba5513a6fc7c5f230788dda91e37
lidor <lidor.borofker@windowslive.com>
2024-01-16 22:22:58 +0200

    test

--- sr7sui/src/King.java
+++ sr7sui/src/King.java
@@ -1,5 +1,5 @@
 public class King extends ConcretePiece{
-    private String type;//testtttttttttttt
+    private String type;//testtttttttttttt987969875765786
     public King(ConcretePiece j67) {
         super(j67.getPosition(), j67.getOwner());
         this.type="king";
