2d12479d3a78841cc13c6cd035aa2958e71e6ee5
Lidx7 <155881749+Lidx7@users.noreply.github.com>
2024-01-16 15:31:36 +0200

    Update Position.java

--- sr7sui/src/Position.java
+++ sr7sui/src/Position.java
@@ -1,8 +1,9 @@
-private int x;
-  private int y;
-public Position(int x,int y){
-    this.x=x;
-    this.y=y;
+public class Position {
+    private int x;
+    private int y;
+    public Position(int x,int y){
+        this.x=x;
+        this.y=y;
     }
     public Position(Position pos)
     {
@@ -10,7 +11,7 @@ public Position(int x,int y){
         this.y=pos.y;
     }
     public int get_x(){
-    return this.x;
+        return this.x;
     }
     public int get_y(){
         return this.y;
@@ -18,3 +19,4 @@ public Position(int x,int y){
     public Position get_position(){
         return new Position(this.x,this.y);
     }
+}
